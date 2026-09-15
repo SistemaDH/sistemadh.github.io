5926c1d439ac870ad58409e8b1f3dfbc60bdd093
m2kjunior <m2kjunior@gmail.com>
2026-09-15 16:11:03 -0300

    chore: fixar motor SRD2 para implantacao

--- supabase/functions/engine-api/index.ts
+++ supabase/functions/engine-api/index.ts
@@ -7,7 +7,7 @@ const cors = {
   "Access-Control-Allow-Methods": "POST, OPTIONS",
 };
 
-const ENGINE_COMMIT = "2761bb828287fe0c17009cf4d0e255ed21762e07";
+const ENGINE_COMMIT = "4fef4d95ced0c3a92cb51eac15f067d2abfadc7c";
 const SOURCE_FILES = [
   "00_Config.gs","30_Personagens.gs","40_Regras.gs","41_Dominios.gs","42_Classes.gs",
   "43_Origens.gs","44_Equipamento.gs","45_Tracos.gs","46_Condicoes.gs","47_Contadores.gs",
